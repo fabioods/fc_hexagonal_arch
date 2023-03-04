64677d3fca749b4cf9406a4322e9a8af0f58b2a1
Fabio dos santos <fah_ds@live.com>
2023-03-04 14:02:44 -0300

    Add cobra cli

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-	db2 "github.com/fabioods/fc_hexagonal_arch/adapters/db"
-	"github.com/fabioods/fc_hexagonal_arch/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/fabioods/fc_hexagonal_arch/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db2.NewProductDB(db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Product test", 10.0)
-	product.UpdatePrice(0)
-	productService.Disable(product)
-
+	cmd.Execute()
 }
